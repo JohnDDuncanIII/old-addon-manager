32199c86b5f2e4300350b34538ae5da0e2a36602
Exalm <exalm7659@gmail.com>
2013-11-23 19:18:13 +0600

    First commit

---
+++ content/appOverlay.js
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+function toEM(aPane) {
+  var theEM = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                        .getService(Components.interfaces.nsIWindowMediator)
+                        .getMostRecentWindow("Extension:Manager");
+  if (theEM) {
+    theEM.focus();
+    if (aPane)
+      theEM.showView(aPane);
+    return;
+  }
+
+  const EMURL = "chrome://mozapps/content/extensions/extensions.xul";
+  const EMFEATURES = "all,dialog=no,height=380,width=720";
+  if (aPane)
+    window.openDialog(EMURL, "", EMFEATURES, aPane);
+  else
+    window.openDialog(EMURL, "", EMFEATURES);
+}
+
+function BrowserOpenAddonsMgr(aView) {
+  if (aView)
+    toEM(aView);
+  else
+    toEM();
+}
+
+function openAddonsMgr(aView) {
+  if (aView)
+    toEM(aView);
+  else
+    toEM();
+}
