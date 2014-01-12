602dee6a953de05c74c48804d8052fea03db1fd4
Exalm <exalm7659@gmail.com>
2014-01-12 20:31:04 +0600

    Added Stylish support

---
+++ content/stylishOverlay.js
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var oamObjectStylish = {
+  updateStylish : function(){
+    var category = gCategories.node.selectedItem.value;
+    var stylish = category == "addons://list/userstyle";
+    document.getElementById("install-style").hidden = !stylish;
+    document.getElementById("new-style").hidden = !stylish;
+    document.getElementById("update-all").hidden = !stylish;
+    if (stylish){
+      document.getElementById("checkUpdatesAllButton").setAttribute("stylish", "true");
+    }else{
+      document.getElementById("checkUpdatesAllButton").removeAttribute("stylish");
+    }
+    document.getElementById("view-port").hidden = stylish;
+    document.getElementById("styles-container").hidden = !stylish;
+  },
+
+  stylishStartInstallFromUrls : function(b){
+    var start = function(){
+      b.setAttribute("disabled", "true");
+    }
+    var end = function(){
+      b.removeAttribute("disabled");
+    }
+    stylishCommon.startInstallFromUrls(start, end);
+  }
+}
+
+window.addEventListener("ViewChanged", oamObjectStylish.updateStylish, false);
